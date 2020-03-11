d0ad0e0a81bdca5ed27791f5e3ef349e510fd6d7
Hai <hohoanghai14894@gmail.com>
2020-03-11 15:21:58 +1100

    version 2, use prompt and add more conditions

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My application is going to tackle a problem lots of people are facing, if not on
 Our early saving and investment is going to dictate whether we're going to have a safe and easy retirement or not. A lots of people having are having trouble saving money in early stage of their career, even later on. Living paycheck by paycheck, spending money you dont have, max out your credits card is not an uncommon lifestyle, especially among younger people.
 
 So in FORTUNE PIGGY, you're going to put in your mothly income, your expenses and your current saving.
-it will return whether you should invest now and if you shouldn't, how long you have to wait until you can invest. If you were ready to invest, it will tell you how much you should put in your investment. all of this is based on your own references.
+it will retyurn whether you should invest now and if you shouldn't, how long you have to wait until you can invest. If you were ready to invest, it will tell you how much you should put in your investment. all of this is based on your own references.
 
 
 
--- project1.rb
+++ project1.rb
@@ -1,30 +1,69 @@
-puts "Hi, what's your name?"
-    name = gets.chomp
-puts " Hi #{name}, so how much do you earn per month?"
-    wages_monthly = gets.chomp.to_i
-puts "Great, so how much is your monthly rent? "
-    rent_monthly = gets.chomp.to_i
-puts "Now, how much do you spend on grocery?"
-    grocery_monthly = gets.chomp.to_i
-puts "How much do you spend on traveling? "
-    travel_monthly = gets.chomp.to_i
-puts "How much do you spend on your bills? "
-    bills_monthly =gets.chomp.to_i
-puts "How much do you spend on your personal things? "
-    personal_monthly = gets.chomp.to_i
-puts "One last thing, how much is on your saving account at the moment?"
-    savings = gets.chomp.to_i
+answer = "yes"
+while answer == "yes"
 
-monthly_expenses = rent_monthly + grocery_monthly + travel_monthly + bills_monthly + personal_monthly
-safety_net = monthly_expenses*6
-# # So my personal financial safety net is 6 months of expenes
-invest_money = savings.to_i - safety_net.to_i
+
+puts "Hi, what's your name?"
+    name = gets.chomp.capitalize
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+wages_monthly = prompt.ask(' So how much is your monthly net income?') do |a|
+    a.in '0-1000000'
+    a.messages[:range?] = "Please enter a valid number"
+  end
+
+  rent_monthly = prompt.ask(' So how much is your monthly rent?') do |b|
+    b.in '0-1000000'
+    b.messages[:range?] = "Please enter a valid number"
+  end
+
+  grocery_monthly = prompt.ask(' So how much do you spend on grocecy monthly?') do |c|
+    c.in '0-1000000'
+    c.messages[:range?] = "Please enter a valid number"
+  end
+  
+  transport_monthly = prompt.ask(' So how much do you spend on your transport monthly?') do |d|
+    d.in '0-1000000'
+    d.messages[:range?] = "Please enter a valid number"
+  end
+
+  bills_monthly = prompt.ask(' So how much are your bills monthly?') do |e|
+    e.in '0-1000000'
+    e.messages[:range?] = "Please enter a valid number"
+  end
+
+  personal_monthly = prompt.ask(' So how much is your personal spending each month?') do |f|
+    f.in '0-1000000'
+    f.messages[:range?] = "Please enter a valid number"
+  end
+
+  savings = prompt.ask(' So how much do you have on your savings at the moment?') do |g|
+    g.in '0-1000000'
+    g.messages[:range?] = "Please enter a valid number"
+  end
+savings = savings.to_i
+wages_monthly = wages_monthly.to_i
+monthly_expenses = (rent_monthly.to_i + grocery_monthly.to_i + transport_monthly.to_i + bills_monthly.to_i + personal_monthly.to_i)
+safety_net = (monthly_expenses)*6
+
+invest_money = savings - safety_net
+if wages_monthly <=  monthly_expenses
+  puts "#{name} you might want to adjust your spending habits, maybe move to a cheaper place, lessen your spending in order to achive your saving goals "
+break 
+end
+
 
 if savings > safety_net 
-     puts "That's great job #{name}, you can now invest #{invest_money}$, keep up the good work!!! "
-elsif savings < safety_net 
-    months_of_work_needed = ((safety_net - savings)/(wages_monthly - monthly_expenses)).round() + 1
-    puts "you need to save for another #{months_of_work_needed} month(s) to be able to invest your money"
-end
-    
+    puts "That's great job #{name}, you can now invest #{invest_money}$, keep up the good work!!! "
 
+elsif savings < safety_net 
+   months_of_work_needed = ((safety_net - savings)/(wages_monthly.to_i - monthly_expenses)).round() + 1
+   puts "#{name} need to save for another #{months_of_work_needed} month(s) to be able to invest your money"
+
+elsif savings == safety_net
+    puts "You can start investing your money now, save for a better future"
+end
+
+puts "Would you like to try again #{name}? (type 'yes' if you do or enter any key if you want to exit)"
+answer = gets.chomp
+end
