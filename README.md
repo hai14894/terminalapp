e2164f1e571a0f060f1013e46b70a2798fea31e7
Hai <hohoanghai14894@gmail.com>
2020-03-12 16:55:09 +1100

    final version

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
-SOFTWARE DEVELOPMENT PLAN (FORTUNE PIGGY)
+<h1>SOFTWARE DEVELOPMENT PLAN (FORTUNE PIGGY)<h1>
 
-FORTUNE PIGGY application will suggest whether you should invest your money or not and how much, bases on how much is your monthly income, your expenses and your current savings.
+<h2>Purpose and scope of the application<h2>
 
-My application is going to tackle a problem lots of people are facing, if not one of the most important aspect of our life, our financial status.
-Our early saving and investment is going to dictate whether we're going to have a safe and easy retirement or not. A lots of people having are having trouble saving money in early stage of their career, even later on. Living paycheck by paycheck, spending money you dont have, max out your credits card is not an uncommon lifestyle, especially among younger people.
+<h3>SO what's the goal of this application<h3>
 
-So in FORTUNE PIGGY, you're going to put in your mothly income, your expenses and your current saving.
-it will retyurn whether you should invest now and if you shouldn't, how long you have to wait until you can invest. If you were ready to invest, it will tell you how much you should put in your investment. all of this is based on your own references.
+FORTUNE PIGGY application will suggest whether you should invest your money or not and how much, bases on your monthly income, your expenses and your current savings.
+
+<h3>What problem will it solve and target audience<h3>
+
+The application is going to tackle a problem lots of people are facing, if not one of the most important aspect of our life, our financial status.
+Our early saving and investment is going to dictate whether we're going to have a safe and easy retirement or not. Making investment early means you're on your way to the ultimate financial goal : FINANCIAL FREEDOM, where you can fund and sustain yourself without working for the rest of your life, where you do something because that's what you like, not what you need to do. A lots of people having are having trouble saving money in early stage of their career, even later on. Living paycheck by paycheck, spending money you dont have, max out your credits card is not an uncommon lifestyle, especially among millennials. And this doesn't mean you have to cut off all your spending in able to do this, it means you have to be consious about your own finance and dont spend more than what you make.
+
+<h3>How it works<h3>
+
+So in FORTUNE PIGGY, you're going to put in your mothly income, your expenses and your current saving, it will calculate the money you need if you're not working for 6 months, in case you lose your job tommorow, that money is called a "safety net"
+It will return whether you should invest now and if you shouldn't, how long you have to wait until you can. If you were ready to invest, it will tell you how much you should put in your investment. All of this is based on your own references.
+
+Link to this repository : git@github.com:hai14894/terminalapp.git
+
+<h2> *FEATURES <h2>
+
+<h3>App logo<h3>
+It will create an app logo when you started running the program by using lolcat and figlet gem.
+<h3>User Input<h3>
+The app is going to ask user to in put their name and numbers for the calculation. If user enters something other and a number when the app asked it will return an error message and it will ask user to enter it again until user enter a valid number. This feature uses tty-prompt gem.
+<h3>rerun the application<h3>
+when it finished the calculation it will ask you if you want to run the app again by typing yes or quit the app by enter any key.
+
+
+
+<h3>Gem used : <h3>
+
+*lolcat - https://rubygems.org/gems/lolcat
+*figlet- https://rubygems.org/gems/figlet
+*tty prompt- https://rubygems.org/gems/tty-prompt
 
 
 
--- project1.rb
+++ project1.rb
@@ -1,3 +1,12 @@
+require 'lolcat'
+require 'figlet'
+def draw()
+  system "clear"
+  system "figlet FORTUNE PIGGY | lolcat"
+
+end
+draw()
+
 answer = "yes"
 while answer == "yes"
 
@@ -7,63 +16,70 @@ puts "Hi, what's your name?"
 require "tty-prompt"
 
 prompt = TTY::Prompt.new
-wages_monthly = prompt.ask(' So how much is your monthly net income?') do |a|
-    a.in '0-1000000'
-    a.messages[:range?] = "Please enter a valid number"
+wages_monthly = prompt.ask('So how much is your monthly net income?') do |a|
+    a.validate /^\d+$/ 
+    a.messages[:valid?] = "Please enter a valid number"
   end
 
-  rent_monthly = prompt.ask(' So how much is your monthly rent?') do |b|
-    b.in '0-1000000'
-    b.messages[:range?] = "Please enter a valid number"
+  rent_monthly = prompt.ask('How much is your monthly rent?') do |b|
+    b.validate /^\d+$/ 
+    b.messages[:valid?] = "Please enter a valid number"
   end
 
-  grocery_monthly = prompt.ask(' So how much do you spend on grocecy monthly?') do |c|
-    c.in '0-1000000'
-    c.messages[:range?] = "Please enter a valid number"
+  grocery_monthly = prompt.ask('How much do you spend on grocecy monthly?') do |c|
+    c.validate /^\d+$/ 
+    c.messages[:valid?] = "Please enter a valid number"
   end
   
-  transport_monthly = prompt.ask(' So how much do you spend on your transport monthly?') do |d|
-    d.in '0-1000000'
-    d.messages[:range?] = "Please enter a valid number"
+  transport_monthly = prompt.ask('How much do you spend on your transport monthly?') do |d|
+    d.validate /^\d+$/ 
+    d.messages[:valid?] = "Please enter a valid number"
   end
 
-  bills_monthly = prompt.ask(' So how much are your bills monthly?') do |e|
-    e.in '0-1000000'
-    e.messages[:range?] = "Please enter a valid number"
+  bills_monthly = prompt.ask('How much are your bills monthly?') do |e|
+    e.validate /^\d+$/ 
+    e.messages[:valid?] = "Please enter a valid number"
   end
 
-  personal_monthly = prompt.ask(' So how much is your personal spending each month?') do |f|
-    f.in '0-1000000'
-    f.messages[:range?] = "Please enter a valid number"
+  personal_monthly = prompt.ask('How much is your personal spending each month?') do |f|
+    f.validate /^\d+$/ 
+    f.messages[:valid?] = "Please enter a valid number"
   end
 
-  savings = prompt.ask(' So how much do you have on your savings at the moment?') do |g|
-    g.in '0-1000000'
-    g.messages[:range?] = "Please enter a valid number"
+  savings = prompt.ask('How much do you have on your savings at the moment?') do |g|
+    g.validate /^\d+$/ 
+    g.messages[:valid?] = "Please enter a valid number"
   end
+
 savings = savings.to_i
 wages_monthly = wages_monthly.to_i
+
 monthly_expenses = (rent_monthly.to_i + grocery_monthly.to_i + transport_monthly.to_i + bills_monthly.to_i + personal_monthly.to_i)
 safety_net = (monthly_expenses)*6
 
 invest_money = savings - safety_net
 if wages_monthly <=  monthly_expenses
+  draw()
   puts "#{name} you might want to adjust your spending habits, maybe move to a cheaper place, lessen your spending in order to achive your saving goals "
 break 
 end
 
 
-if savings > safety_net 
+if savings > safety_net
+   draw()
     puts "That's great job #{name}, you can now invest #{invest_money}$, keep up the good work!!! "
 
 elsif savings < safety_net 
-   months_of_work_needed = ((safety_net - savings)/(wages_monthly.to_i - monthly_expenses)).round() + 1
-   puts "#{name} need to save for another #{months_of_work_needed} month(s) to be able to invest your money"
+   months_of_work_needed = ((safety_net - savings)/(wages_monthly.to_i - monthly_expenses)).ceil
+   draw()
+   puts "#{name} you need to save for another #{months_of_work_needed} month(s) to be able to invest your money"
 
 elsif savings == safety_net
+  draw()
     puts "You can start investing your money now, save for a better future"
 end
 
-puts "Would you like to try again #{name}? (type 'yes' if you do or enter any key if you want to exit)"
+puts "Would you like to do caculation again? (type 'yes' if you do or enter any key to exit)"
 answer = gets.chomp
 end
+
